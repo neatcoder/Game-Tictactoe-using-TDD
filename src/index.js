395bc196ffb877723b5ed4487655563685d53de5
singhsaab <vikrammrce@gmail.com>
2019-07-08 01:04:30 +0200

    Refactor: HTML grid for TicTacToe created via JS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-import TicTacToe from './views/TicTacToe';
-import htmlTemplate from './htmlTemplates/tictactoe.html';
+var TicTacToe = require('./views/TicTacToe');
 
-document.body.innerHTML=htmlTemplate;
+function makeTicTacToeGameGrid() {
+    document.body.innerHTML='<div class="grid-container" id="grid"></div>';
+    for(i=0; i<9; i++){
+        document.getElementById('grid').appendChild('<div class="grid-item"></div>');
+    }
+}
 
+makeTicTacToeGameGrid();
 var tictactoe = new TicTacToe();

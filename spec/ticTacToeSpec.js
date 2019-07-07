42cd3e60bb7a0b4e771d3a0e8f7374e294695f6d
singhsaab <vikrammrce@gmail.com>
2019-07-08 01:41:26 +0200

    Feature: scores for both players should be 0

---
+++ spec/ticTacToeSpec.js
@@ -0,0 +1,13 @@
+var TicTacToe = require('../src/views/ticTacToe');
+
+describe("TicTacToe Game", ()=>{
+    var ticTacToe;
+    beforeEach(()=>{
+        ticTacToe = new TicTacToe();
+    });
+
+    it("should score be 0 for both players in the beginning of the match", ()=>{
+        expect(ticTacToe.scoreOfPlayerX).toBe(0);
+        expect(ticTacToe.scoreOfPlayerO).toBe(0);
+    });
+})
--- src/views/ticTacToe.js
+++ src/views/ticTacToe.js
@@ -1,6 +1,7 @@
 class TicTacToe {
     constructor() {
-        
+        this.scoreOfPlayerX = 0;
+        this.scoreOfPlayerO = 0;
     }
 };
 module.exports = TicTacToe;
